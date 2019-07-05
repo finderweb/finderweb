7853414c39bf86ba9c9df41e51ecead117b5e975
Rahul Bansal <bansal.rahul14@gmail.com>
2019-07-05 22:04:34 +0530

    added mock

--- application.py
+++ application.py
@@ -1,5 +1,55 @@
 from flask import Flask
+
 app = Flask(__name__)
+
+
+@app.route("api/signup")
+def signup_mock():
+    return {
+        "name": "rahul",
+        "number": "7897580575"
+    }
+
+
+@app.route("api/contacts")
+def contacts_mock():
+    return {
+        "name": "rahul",
+        "number": "7897580575"
+    }
+
+
+@app.route("api/search")
+def search_mock():
+    return {
+        "sourceNumber": "07897580575",
+        "sourceName": "Rahul",
+        "destinationNumber": "09210022557",
+        "destinationName": "Papa",
+        "edges": [
+            {
+                "nodeNumber": "07897580575",
+                "contactName": "Rahul",
+                "order": 0
+            },
+            {
+                "nodeNumber": "07897580575",
+                "contactName": "Rahul Bansal",
+                "order": 1
+            },
+            {
+                "nodeNumber": "07897580575",
+                "contactName": "Rahul 2",
+                "order": 3
+            },
+            {
+                "nodeNumber": "0897580575",
+                "contactName": "Rahul 3",
+                "order": 2
+            }
+        ]
+    }
+
 
 @app.route("/")
 def hello():
